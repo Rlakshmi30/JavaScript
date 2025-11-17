cbe0cf89c24eab85bb9e3e5863d9a65524104744
Rlakshmi30 <r.lakshmi0048@gmail.com>
2025-11-18 03:11:43 +0530

    updated js operators

---
+++ operators.js
@@ -0,0 +1,19 @@
+// Arithmetic Operators:
+let a = 5;
+let b = 30;
+let c = 9;
+console.log("a =", a, " & b =", b);
+
+console.log("a + b =", a + b);// + (increment/Sum):
+console.log("b - a =", b - a);// - (decrement/substraction): 
+console.log("a * b =", a * b);// * (multiplication):
+console.log("b / a =", b / a);// / (division):
+console.log("b % c =", b % c);// % (modulus (remainder)):
+console.log("a to the power c", a ** c);// **(exponential)
+
+
+
+
+
+
+
