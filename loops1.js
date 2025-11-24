cb9b16809185d17e1f8e11d86d0654980bf11cd6
Rlakshmi30 <r.lakshmi0048@gmail.com>
2025-11-24 17:46:49 +0530

    updatedLoops

--- loops1.js
+++ loops1.js
@@ -4,3 +4,7 @@ for (let num = 1; num <= 5; num++) {
     console.log(num);
 
 }
+for (let a = 22; a <= 29; a++) {
+    console.log(a);
+
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,10 +1,8 @@
-let sum = 0;
+/*let sum = 0;
 for (let num = 1; num <= 5; num++) {
     sum += num;
 }
-console.log("sum =", sum);
-
-
+console.log("sum =", sum);*/
 let sum2 = 0;
 for (let a = 1; a <= 10; a += 2) {
     console.log(a);
