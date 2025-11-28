1aa57a5ffb72d99357eaf97f64a9943fc0c5e17d
Rlakshmi30 <r.lakshmi0048@gmail.com>
2025-11-28 23:28:49 +0530

    Updated while loops

---
+++ loops_while.js
@@ -0,0 +1,16 @@
+//While loop:
+
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+
+let i2 = 1;
+let sum = 0;
+while (i2 <= 10) {
+    if (i2 % 2 === 0) sum += i2;
+    i2++;
+}
+console.log(sum);
