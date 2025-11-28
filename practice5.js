c1aa37c28f5364575ad3a77c0983c3b00eee63fa
Rlakshmi30 <r.lakshmi0048@gmail.com>
2025-11-28 23:42:32 +0530

    updated

---
+++ practice5.js
@@ -0,0 +1,27 @@
+let i = 1;
+
+while (i <= 10) {
+    console.log(i * i);
+    i++;
+}
+
+
+let i2 = 1;
+let sum = 0;
+
+while (i2 <= 50) {
+    sum += i2;
+    i2++;
+}
+
+console.log("Sum =", sum);
+
+
+let x = 1;
+
+while (x <= 30) {
+    if (x % 2 !== 0) {
+        console.log(x);
+    }
+    x++;
+}
